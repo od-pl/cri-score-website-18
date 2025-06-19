31200f84af9c91418876f6f6b5e2e78ba61f9468
rikkot <username@gmail.com>
2025-06-19 11:21:44 +0530

    Merge with lovable edits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Mail, Phone, MapPin, Linkedin, Twitter, Globe } from "lucide-react";
+// import { Mail, Phone, MapPin, Linkedin, Twitter, Globe } from "lucide-react";
 
 const Footer = () => {
   return (
